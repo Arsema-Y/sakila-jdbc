0910647f7ea9f8d4a5d4c9b86eecd029c559eade
mili Arsema <meleymm88@gmail.com>
2026-06-04 13:17:27 -0500

    Creats and configures Data Source

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,7 +1,22 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class App {
+    /*
+    1. Creat dataSource
+    2. Open con
+    3. PrepStat.
+    4. Query
+    5. ResultSet
+    6. close res.
+
+     */
     public static void main(String[] args) {
 
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
     }
 }
